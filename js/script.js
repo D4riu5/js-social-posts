32bcabb5bc5c95876870b74458c934f0a5e7cee1
D4riu5 <R3iji94@gmail.com>
2023-01-18 17:36:13 +0100

    completed milestones 1 and 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// arrays
 const posts = [
   {
       "id": 1,
@@ -16,7 +17,7 @@ const posts = [
       "media": "https://unsplash.it/600/400?image=112",
       "author": {
           "name": "Sofia Perlari",
-          "image": "https://unsplash.it/300/300?image=10"
+          "image": null
       },
       "likes": 120,
       "created": "2021-09-03"
@@ -35,7 +36,7 @@ const posts = [
   {
       "id": 4,
       "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-      "media": "https://unsplash.it/600/400?image=24",
+      "media": null,
       "author": {
           "name": "Luca Formicola",
           "image": null
@@ -55,3 +56,48 @@ const posts = [
       "created": "2021-03-05"
   }
 ];
+
+// selectors
+const container = document.getElementById("container");
+
+// inserting objects on page
+for (let post of posts){
+  container.innerHTML += 
+  `
+    <div class="post rounded">
+                
+      <div class="post__header">
+          <div class="post-meta">                    
+              <div class="post-meta__icon">
+                  <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                     
+              </div>
+              <div class="post-meta__data">
+                  <div class="post-meta__author">${post.author.name}</div>
+                  <div class="post-meta__time">${post.created}</div>
+              </div>                    
+          </div>
+      </div>
+
+      <div class="post__text">${post.content}
+      </div>
+
+      <div class="post__image">
+          <img src="${post.media}" alt="">
+      </div>
+
+      <div class="post__footer">
+          <div class="likes js-likes">
+              <div class="likes__cta">
+                  <a class="like-button js-like-button" href="javascript:void(0)" data-postid="${post.id}">
+                      <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                      <span class="like-button__label">Mi Piace</span>
+                  </a>
+              </div>
+              <div class="likes__counter">
+                  Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+              </div>
+          </div> 
+      </div>            
+    </div>
+  `;
+}
